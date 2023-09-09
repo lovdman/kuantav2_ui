d07a888bfb050436c9fa9791292562b2bf58ac3d
aui_tuthousend <mraihan286@gmail.com>
2023-09-09 18:42:35 +0700

    add detail kegiatan button in homepage

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -5,7 +5,7 @@
         <h2 class="mb-0">List Kegiatan</h2>
 {{--        <a href='/addtask' class="btn btn-dark">Add Task + </a>--}}
     </div>
-    <table class="table table-hover mt-3">
+    <table class="table table-hover table-bordered mt-3">
         <thead class="table-primary">
         <tr style="text-align: center;">
             <th>Judul Program</th>
@@ -18,17 +18,18 @@
         </tr>
         </thead>
         <tbody>
-                <tr style="text-align: center;">
-                    <td class="align-middle">ini judul prog</td>
-                    <td class="align-middle">School Design</td>
-                    <td class="align-middle">ini judul keg</td>
-                    <td class="align-middle">5 M</td>
-                    <td class="align-middle">ini catatan</td>
-                    <td class="align-middle">ini capaian</td>
-                    <td class="align-middle">
-                        <a href="#" class="btn btn-info" data-toggle="modal" data-target="#update">update capaian</a>
-                    </td>
-                </tr>
+        <tr style="text-align: center;">
+            <td class="align-middle">ini judul prog</td>
+            <td class="align-middle">School Design</td>
+            <td class="align-middle">ini judul keg</td>
+            <td class="align-middle">5 M</td>
+            <td class="align-middle">ini catatan</td>
+            <td class="align-middle">ini capaian</td>
+            <td class="align-middle">
+                <a href="#" class="btn btn-info" data-toggle="modal" data-target="#update">update capaian</a>
+                <a href="{{ route('kegiatan', "programnya apa") }}" class="btn btn-info">detail</a>
+            </td>
+        </tr>
         </tbody>
     </table>
 
